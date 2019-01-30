e1ba9021bcb830599c513ac8c1318607835d52d9
Olgac Zaim <46277089+olgaczaim@users.noreply.github.com>
2019-01-30 16:48:28 +0300

    update

--- server.js
+++ server.js
@@ -2,23 +2,36 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
-
+//for running hbs footer
+//nodemon server.js -e js,hbs
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',(req, res) => {
-    //res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Olgac',
-        likes: [
-            'Biking',
-            'Movies'
-        ]
+//app.get('/',(req, res) => {
+//    //res.send('<h1>Hello Express!</h1>');
+//    res.send({
+//        name: 'Olgac',
+//        likes: [
+//            'Biking',
+//            'Movies'
+//        ]
+//    });
+//});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) =>{
