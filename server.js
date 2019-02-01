1a93c05e3f3b5d9c16203306b5f2917e3f6b13ee
Olgac Zaim <46277089+olgaczaim@users.noreply.github.com>
2019-02-01 15:50:41 +0300

    update

--- server.js
+++ server.js
@@ -67,6 +67,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    })
+});
+
 app.get('/bad', (req, res) =>{
     res.send({
         errorMssage: 'Unable to handle request'
